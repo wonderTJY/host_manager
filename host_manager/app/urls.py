c10ab3dce76c9a634cf6621f57d7ad1eed135d23
Comintern <1683096594@qq.com>
2025-06-02 00:05:48 +0800

    主机管理V1.0.0

--- host_manager/app/urls.py
+++ host_manager/app/urls.py
@@ -9,12 +9,7 @@ router.register(r'datacenters', views.DataCenterViewSet)
 router.register(r'hosts', views.HostViewSet)
 router.register(r'statistics', views.HostStatisticViewSet)
 
-# 应用特定的URL模式
 app_name = 'host_app'
 urlpatterns = [
-    # DRF路由器生成的路由
     path('', include(router.urls)),
-
-    # 如果需要添加额外的非视图集路由
-    # path('custom-endpoint/', views.custom_view, name='custom-view'),
 ]
